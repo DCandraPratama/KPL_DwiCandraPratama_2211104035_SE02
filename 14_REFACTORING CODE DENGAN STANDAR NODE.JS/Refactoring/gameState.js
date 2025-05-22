9a56f3a9670c4d903e64172a00a7b4dc84a71080
DCandraPratama <dwicandrapratama4@gmail.com>
2025-05-22 22:54:35 +0700

    Clean Code

---
+++ 14_REFACTORING CODE DENGAN STANDAR NODE.JS/Refactoring/gameState.js
@@ -0,0 +1,15 @@
+// gameState.js
+
+/**
+ * Enum State Game
+ * @readonly
+ * @enum {string}
+ */
+const GameState = {
+    START: 'START',
+    PLAYING: 'PLAYING',
+    GAME_OVER: 'GAME_OVER',
+    EXIT: 'EXIT',
+  };
+  
+  module.exports = GameState;
